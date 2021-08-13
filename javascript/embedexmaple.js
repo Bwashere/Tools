19ea3198c713b281570d235c7c281fd363501cee
Brackzz <87747715+Bwashere@users.noreply.github.com>
2021-08-13 17:54:28 -0500

    Create embedexmaple.js

---
+++ javascript/embedexmaple.js
@@ -0,0 +1,6 @@
+const bam = new Discord.MessageEmbed()
+	.setTitle('Some title')
+.setImage('Bam')
+.setFooter('woohs')
+channel.send(bam)
+// Make sure to read da docs LOL if you dont understand this and learn basic javscript 
