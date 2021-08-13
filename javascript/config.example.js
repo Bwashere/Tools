ef42f91e23545d8b700f64a9fda8fb5e2dae0b29
Brackzz <87747715+Bwashere@users.noreply.github.com>
2021-08-13 17:51:37 -0500

    Bam

---
+++ javascript/config.example.js
@@ -0,0 +1,5 @@
+const config = {
+    token: "ODc1Nzk4OTA1Mzk5NDEwNzI4.YRaxSg.hwzIc0IrJOgHkXpD4dzX2leX1CA" // 
+}
+I would use this instead of a config.json because in json files there are no comments so it makes life harder
+module.exports = config; // This is needed for da exports
